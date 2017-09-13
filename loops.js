706f42fc12c9ef89da383f3ff0793b541840d6d3
Crista Cooper <aine3d@gmail.com>
2017-09-13 18:12:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,20 @@
 
 
-function whileLoop(number) {
+//function whileLoop(number) {
 
-  while (number <= 20) {
-    console.log(number);
-    number -= 1;
+//  while (number <= 20) {
+//    console.log(number);
+//    number -= 1;
+//  }
+//  console.log('done');
+//}
+var array = [];
+
+function forLoop(array) {
+  for (var i = 0, i <25, i += 1);
+    if (i = 1) {
+    console.log('I am 1 strange loop.')
+  } else {
+    console.log("I am ${i} strange loops")
   }
-  console.log('done');
-}
+};

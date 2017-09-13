c3b8b126d9b102bcbec2555bc7bf2e5dafeae6cf
Crista Cooper <aine3d@gmail.com>
2017-09-13 18:28:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,11 +2,12 @@
 
 function forLoop(array) {
 
-  for (var i = 0, i <25, i += 1);
+  for (let i = 0, i <25, i += 1);
     if (i === 1) {
     array.push('I am 1 strange loop.')
   } else {
-    array.push('I am ${i} strange loops')
+    array.push('I am ${i} strange loops.')
   }
-  return array
+}
+return array;
 }
